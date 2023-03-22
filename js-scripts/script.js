ebbf1299aa790a1061ed19fbbdb95962561a5c53
rafanthx13 <rafaassis15@gmail.com>
2023-03-22 20:47:21 -0300

    conseguiu colocar o lsitener em tudo

--- js-scripts/script.js
+++ js-scripts/script.js
@@ -1,27 +1,54 @@
 /* Seleciona os elementos  HTML do chat.php */
-const sendBtn = document.querySelector("#send-form");
+const sendBtn = document.querySelector(".send-form");
 const form = document.querySelector(".typing-area");
 const originalImage = form.querySelector("#original-image").value;
 const prefixer = form.querySelector("#prefixer").value;
 
-sendBtn.onclick = () => {
-	console.log('entrou')
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", "php-scripts/alter-image.php", true);
-	console.log(form);
-	xhr.onload = () => {
-		if(xhr.readyState === XMLHttpRequest.DONE){
-			console.log('success')
-			// if(xhr.status === 200){
-			// 	inputField.value = "";
-			// 	scrollToBottom();
-			// }
-		}
-	}
+var cards = document.querySelectorAll(".send-form");
+for (var i = 0; i < cards.length; i++) {
+    var card = cards[i];
+    formre = card.parentNode
+    card.onclick = function () {
+    	let xhr = new XMLHttpRequest();
+			xhr.open("POST", "php-scripts/alter-image.php", true);
+			formre = this.parentNode
+			// console.log(this.parentNode);
+			xhr.onload = () => {
+				if(xhr.readyState === XMLHttpRequest.DONE){
+					console.log('success')
+					// if(xhr.status === 200){
+					// 	inputField.value = "";
+					// 	scrollToBottom();
+					// }
+				}
+			}
 
-	let formData = new FormData(form);
-	for (const [key, value] of formData) {
-	  console.log(`${key}: ${value}\n`);
-	}	
-	xhr.send(formData);
+			let formData = new FormData(formre);
+			for (const [key, value] of formData) {
+			  console.log(`${key}: ${value}\n`);
+			}	
+			xhr.send(formData);
+    };
 }
+
+// sendBtn.onclick = () => {
+// 	console.log('entrou')
+// 	let xhr = new XMLHttpRequest();
+// 	xhr.open("POST", "php-scripts/alter-image.php", true);
+// 	console.log(form);
+// 	xhr.onload = () => {
+// 		if(xhr.readyState === XMLHttpRequest.DONE){
+// 			console.log('success')
+// 			// if(xhr.status === 200){
+// 			// 	inputField.value = "";
+// 			// 	scrollToBottom();
+// 			// }
+// 		}
+// 	}
+
+// 	let formData = new FormData(form);
+// 	for (const [key, value] of formData) {
+// 	  console.log(`${key}: ${value}\n`);
+// 	}	
+// 	xhr.send(formData);
+// }
