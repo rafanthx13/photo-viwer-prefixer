7a8c1b5ac312a37027f05b81cb280296dbc2bd04
Rafael M Assis <rafa-assis@hotmail.com>
2025-03-09 19:15:45 -0300

    ere

--- js-scripts/autocomplete.js
+++ js-scripts/autocomplete.js
@@ -3,65 +3,71 @@ function autocomplete(inp, arr) {
   the text field element and an array of possible autocompleted values:*/
   var currentFocus;
   /*execute a function when someone writes in the text field:*/
-  inp.addEventListener("input", function(e) {
-      var a, b, i, val = this.value;
-      /*close any already open lists of autocompleted values*/
-      closeAllLists();
-      if (!val) { return false;}
-      currentFocus = -1;
-      /*create a DIV element that will contain the items (values):*/
-      a = document.createElement("DIV");
-      a.setAttribute("id", this.id + "autocomplete-list");
-      a.setAttribute("class", "autocomplete-items");
-      /*append the DIV element as a child of the autocomplete container:*/
-      this.parentNode.appendChild(a);
-      /*for each item in the array...*/
-      for (i = 0; i < arr.length; i++) {
-        /*check if the item starts with the same letters as the text field value:*/
-        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-          /*create a DIV element for each matching element:*/
-          b = document.createElement("DIV");
-          /*make the matching letters bold:*/
-          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-          b.innerHTML += arr[i].substr(val.length);
-          /*insert a input field that will hold the current array item's value:*/
-          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-          /*execute a function when someone clicks on the item value (DIV element):*/
-          b.addEventListener("click", function(e) {
-              /*insert the value for the autocomplete text field:*/
-              inp.value = this.getElementsByTagName("input")[0].value;
-              /*close the list of autocompleted values,
+  inp.addEventListener("input", function (e) {
+    var a,
+      b,
+      i,
+      val = this.value;
+    /*close any already open lists of autocompleted values*/
+    closeAllLists();
+    if (!val) {
+      return false;
+    }
+    currentFocus = -1;
+    /*create a DIV element that will contain the items (values):*/
+    a = document.createElement("DIV");
+    a.setAttribute("id", this.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+    /*append the DIV element as a child of the autocomplete container:*/
+    this.parentNode.appendChild(a);
+    /*for each item in the array...*/
+    for (i = 0; i < arr.length; i++) {
+      /*check if the item starts with the same letters as the text field value:*/
+      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        /*create a DIV element for each matching element:*/
+        b = document.createElement("DIV");
+        /*make the matching letters bold:*/
+        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+        b.innerHTML += arr[i].substr(val.length);
+        /*insert a input field that will hold the current array item's value:*/
+        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+        /*execute a function when someone clicks on the item value (DIV element):*/
+        b.addEventListener("click", function (e) {
+          /*insert the value for the autocomplete text field:*/
+          inp.value = this.getElementsByTagName("input")[0].value;
+          /*close the list of autocompleted values,
               (or any other open lists of autocompleted values:*/
-              closeAllLists();
-          });
-          a.appendChild(b);
-        }
+          closeAllLists();
+        });
+        a.appendChild(b);
       }
+    }
   });
   /*execute a function presses a key on the keyboard:*/
-  inp.addEventListener("keydown", function(e) {
-      var x = document.getElementById(this.id + "autocomplete-list");
-      if (x) x = x.getElementsByTagName("div");
-      if (e.keyCode == 40) {
-        /*If the arrow DOWN key is pressed,
+  inp.addEventListener("keydown", function (e) {
+    var x = document.getElementById(this.id + "autocomplete-list");
+    if (x) x = x.getElementsByTagName("div");
+    if (e.keyCode == 40) {
+      /*If the arrow DOWN key is pressed,
         increase the currentFocus variable:*/
-        currentFocus++;
-        /*and and make the current item more visible:*/
-        addActive(x);
-      } else if (e.keyCode == 38) { //up
-        /*If the arrow UP key is pressed,
+      currentFocus++;
+      /*and and make the current item more visible:*/
+      addActive(x);
+    } else if (e.keyCode == 38) {
+      //up
+      /*If the arrow UP key is pressed,
         decrease the currentFocus variable:*/
-        currentFocus--;
-        /*and and make the current item more visible:*/
-        addActive(x);
-      } else if (e.keyCode == 13) {
-        /*If the ENTER key is pressed, prevent the form from being submitted,*/
-        e.preventDefault();
-        if (currentFocus > -1) {
-          /*and simulate a click on the "active" item:*/
-          if (x) x[currentFocus].click();
-        }
+      currentFocus--;
+      /*and and make the current item more visible:*/
+      addActive(x);
+    } else if (e.keyCode == 13) {
+      /*If the ENTER key is pressed, prevent the form from being submitted,*/
+      e.preventDefault();
+      if (currentFocus > -1) {
+        /*and simulate a click on the "active" item:*/
+        if (x) x[currentFocus].click();
       }
+    }
   });
   function addActive(x) {
     /*a function to classify an item as "active":*/
@@ -69,7 +75,7 @@ function autocomplete(inp, arr) {
     /*start by removing the "active" class on all items:*/
     removeActive(x);
     if (currentFocus >= x.length) currentFocus = 0;
-    if (currentFocus < 0) currentFocus = (x.length - 1);
+    if (currentFocus < 0) currentFocus = x.length - 1;
     /*add class "autocomplete-active":*/
     x[currentFocus].classList.add("autocomplete-active");
   }
@@ -91,23 +97,61 @@ function autocomplete(inp, arr) {
   }
   /*execute a function when someone clicks in the document:*/
   document.addEventListener("click", function (e) {
-      closeAllLists(e.target);
+    closeAllLists(e.target);
   });
 }
 
 /*An array containing all the country names in the world:*/
 var nome_to_complete = [
-    "dompatcho", "pedro7c", "icaro", "italo", "italo-segundo", "tabnews",
-    "astrologia", "tmdz",  "candidates", "licoes",
-    
-    "@thiago2n", 
+  "dompatcho",
+  "pedro7c",
+  "icaro",
+  "italo",
+  "italo-segundo",
+  "tabnews",
+  "astrologia",
+  "tmdz",
+  "candidates",
+  "licoes",
+
+  "@thiago2n",
 ];
 
-/*initiate the autocomplete function on the "prefixer" element, and pass along the nome_to_complete array as possible autocomplete values:*/
-autocomplete(document.getElementById("prefixer"), nome_to_complete);
+function lerTags(arquivo) {
+  fetch(arquivo)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erro ao carregar o arquivo");
+      }
+      return response.text();
+    })
+    .then((texto) => {
+      const tags = texto
+        .split("\n")
+        .map((tag) => tag.trim())
+        .filter((tag) => tag !== "");
+      console.log(tags);
+      /*initiate the autocomplete function on the "prefixer" element, and pass along the nome_to_complete array as possible autocomplete values:*/
+      autocomplete(document.getElementById("prefixer"), nome_to_complete);
 
-var cards = document.querySelectorAll(".prefixer-func");
-for (var i = 0; i < cards.length; i++) {
-    var card = cards[i];
-    autocomplete(card, nome_to_complete);
+      var cards = document.querySelectorAll(".prefixer-func");
+      for (var i = 0; i < cards.length; i++) {
+        var card = cards[i];
+        autocomplete(card, nome_to_complete);
+      }
+    })
+    .catch((erro) => {
+      console.error("Erro:", erro);
+    });
 }
+
+lerTags("tags.txt");
+
+// /*initiate the autocomplete function on the "prefixer" element, and pass along the nome_to_complete array as possible autocomplete values:*/
+// autocomplete(document.getElementById("prefixer"), nome_to_complete);
+
+// var cards = document.querySelectorAll(".prefixer-func");
+// for (var i = 0; i < cards.length; i++) {
+//     var card = cards[i];
+//     autocomplete(card, nome_to_complete);
+// }
